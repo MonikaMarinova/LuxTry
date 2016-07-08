2f052580204e85e6049f5d4ae1c207c42819ffad
Monika Marinova <monikasmarinova@gmail.com>
2016-07-08 14:30:45 +0300

    Exception fro String

---
+++ invalidparameterexception.h
@@ -0,0 +1,10 @@
+#ifndef INVALIDPARAMETEREXCEPTION_H
+#define INVALIDPARAMETEREXCEPTION_H
+
+class InvalidParameterException: public exception
+{
+public:
+    InvalidParameterException();
+};
+
+#endif // INVALIDPARAMETEREXCEPTION_H
