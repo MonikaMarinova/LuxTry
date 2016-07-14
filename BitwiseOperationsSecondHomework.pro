7b5cfdf874b3ce4f0dcaf0ea33ab2bf723a41b5e
Niksan9502 <Niksan_boy@abv.bg>
2016-07-14 16:29:56 +0300

    bitwise 14.07.16

---
+++ BitwiseOperationsSecondHomework.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-07-14T11:26:45
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = BitwiseOperationsSecondHomework
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
--- New.cpp
+++ New.cpp
@@ -1,6 +1,5 @@
 
 #include <iostream>
-//#include "New.cpp"
 /*
 #define firstBit        0b1
 #define secondBit       0b10
@@ -33,25 +32,20 @@
 #define                 0b10000000000000000000000000000
 #define                 0b100000000000000000000000000000
 */
-void checkbits1(int sequence[]);
-void checkbits(int sequence[]);
-int i;
-//int flag=0;
+//void checkbits1(int sequence[]);
+void func();
 
-int main()
-{
-    int sequence  [32] = {272, 272, 224, 16252928, 50593792, 33685504, 67239936, 67174400, 33587200, 16809984, 16973824, 8650752, 7864320, 0};
-    int sequence1 [32] = {0, 224, 0, 0, 16252928, 33292288, 66846720, 66977792, 33488896, 16711680, 16515072, 7864320, 0, 0};
-    checkbits(sequence);
-    return 0;
-}
-/*
+
+
+//long check=2147483648;
+
+
 void checkbits1(int sequence[])
 {
-    int flag=1;
+    int flag=0;
     std::cout<<"Input\t\tInput as Grid\n";
 
-    for( i=0;i<32;i++)
+    for(int i=0;i<32;i++)
     {
         std::cout<<sequence[i];
         if(sequence[i]<10000000)
@@ -63,24 +57,15 @@ void checkbits1(int sequence[])
         for(long check=2147483648;check>0;check/=2) //
         {
 
-            if(((sequence[i]&check)==check) && flag==1)
-            {
-                std::cout<<"1";
-                flag=0;
-            }
-            else if((((sequence[i]&check)==check)) || flag==0)
-            {
-                std::cout<<".";
-                //flag=0;
-            }
-            else if(((((sequence[i]&check)==check)) && flag==0))
+            if(((sequence[i]&check)==check) && flag==0)
                 flag=1;
+            else if(((sequence[i]&check)==check) && flag==1)
+                flag=0;
                 //void newOne();
-            /*if(flag==1)
+            if(flag==1)
                 std::cout<<1;
-            else if(flag==0)
+            else
                 std::cout<<".";
-
         }
         std::cout<<"\n";
     }
@@ -129,87 +114,6 @@ void func()
         if(!(((sequence[i]&check)==check)))
             flag=0;
 
-    }
-}
-*/
-
-//------------
-
-void checkbits(int sequence[])
-{
-    std::cout<<"Input\t\tInput as Grid\n";
-    bool a;
-    int flag=0;
-    for(i=0;i<32;i++)
-    {
-        std::cout<<sequence[i];
-        if(sequence[i]<10000000)
-        {
-            std::cout<<"\t\t";
-        }
-        else if(sequence[i]<100000000)
-            std::cout<<"\t";
-        for(long check=2147483648;check>0;check/=2) //
-        {
-            a=(sequence[i]&check)==check;
-            if(a==0 && flag==0)
-                std::cout<<".";
-            else if(a==1 && flag==0)
-            {
-                std::cout<<".";
-                flag=1;
-                for(;flag!=0 || check !=0;check/=2)
-                {
-                    a=(sequence[i]&check)==check;
-                    if(a==1 && flag==1)
-                        std::cout<<1;
-                    else if(a==0 && flag==1)
-                    {
-                        std::cout<<".";
-                        flag=0;
-                    }
-                }
-            }/*
-            if(a && flag==1)
-                flag=0;
-            //else
-            //    flag=0;
-            else if(a)
-                flag=1;
-                //sequence[i]==sequence[i]^check;
-                //std::cout<<".";
-            //else id ()
-              //  std::cout<<1;
-*/
-        }
-        std::cout<<"\n";
-    }
-}
-
-//-----------
-/*
-void checkbits(int sequence[])
-{
-    std::cout<<"Input\t\tInput as Grid\n";
-
-    for(i=0;i<32;i++)
-    {
-        std::cout<<sequence[i];
-        if(sequence[i]<10000000)
-        {
-            std::cout<<"\t\t";
-        }
-        else if(sequence[i]<100000000)
-            std::cout<<"\t";
-        for(long check=2147483648;check>0;check/=2) //
-        {
-
-            if((sequence[i]&check)==check)
-                std::cout<<1;
-            else
-                std::cout<<".";
-        }
-        std::cout<<"\n";
     }
 }
 */
